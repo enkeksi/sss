1168247872a6807959e8fa2ede10c9ad34afa5c0
enkeksi <dhr.burmeister@gmail.com>
2024-09-26 13:14:34 +0300

    Add files via upload

---
+++ Java beans/newJavaFile2.java
@@ -0,0 +1,13 @@
+
+ 
+public class newJavaFile2 {
+
+    public static void main(String args[]) {
+  String namn = "daniel";
+  String efternamn = "Burmeister";
+  int age = 17;
+  double height = 1.87;
+  boolean student = true;
+        System.out.println(namn+age);
+    }
+}
